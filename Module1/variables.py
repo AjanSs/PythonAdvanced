31c6d7e7ddc11ee01bf9523f7a3e5e10d9d319db
Ajan <ajansyla2@gmail.com>
2025-11-08 14:33:03 +0100

    lesson1

---
+++ Module1/variables.py
@@ -0,0 +1,22 @@
+name = "Ajan"
+print(name)
+
+temperature = 98.6
+print(temperature)
+print(type(temperature))
+
+age = 30
+print("age is", age)
+
+x = 5
+y = 3
+result = x + y
+print("resultati eshte", result)
+
+age = age + 1
+print("age now is", age)
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
