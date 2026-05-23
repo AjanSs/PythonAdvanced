110f4a1f8e5719fbb3ddd6262de8cafe17e74539
AjanSs <ajansyla2@gmail.com>
2026-05-23 14:44:59 +0200

    22
    22

---
+++ Module 22/api_development/api.py
@@ -0,0 +1,27 @@
+from idlelib.rpc import response_queue
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+    email: str
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+class PersonResponse(BaseModel):
+    message: str
+
+@app.post("/users/")
+async def create_users(user: User):
+    return user
+
+@app.post("/create_person/", response_model= PersonResponse)
+async def create_person(person: Person):
+    return {"message":f"Person {person.name} create with age{person.age}"}
